43842dae17b548c6bf19388f171c40e0c5502614
pan <edenpan91@gmail.com>
2025-09-18 01:42:27 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -19,8 +19,13 @@ export default defineConfig({
     },
   },
   server: {
-    host: true,   // 允许外部访问（0.0.0.0）
+    // host: true,   // 允许外部访问（0.0.0.0）
+    host: '0.0.0.0'
     port: 5173,   // 指定端口
+    hmr:{
+      host: '192.168.50.216',
+      protocol: 'ws',
+    }
   },
 })
 
